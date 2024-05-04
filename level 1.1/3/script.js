4c7efd4887fa4f74fc710edccde1931f54967a8c
ChAmmar28 <niyazbekovammar@gmail.com>
2024-05-04 21:55:47 +0600

    little remark

--- level 1.1/3/script.js
+++ level 1.1/3/script.js
@@ -4,7 +4,7 @@ let string = prompt("Введите строку", "");
 let func = (x) => {
   if (x) {
     // alert(string.slice(-1));// first
-    alert(string.at(-1)); // second
+    alert(string.at(-1)); // second "faster because part of ECMAScript"
   } else {
     alert("empty");
   }
