a2ca56e312e3dd03bb561c34a21f84d0f2467423
ChAmmar28 <niyazbekovammar@gmail.com>
2024-05-10 00:25:01 +0600

    done

---
+++ level 1.2/3/script.js
@@ -0,0 +1,11 @@
+// Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
+
+let func = (x) => {
+  //   return Number(x.at(0)) + Number(x.at(-1)); // мой вариант
+  //   return parseInt(x.at(0)) + parseInt(x.at(-1)); //подсказка от gpt "paraseInt()"
+  if (x) {
+    return parseInt(x.at(0)) + parseInt(x.at(-1));
+  }
+};
+
+console.log(func(prompt("", 0)));
