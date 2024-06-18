cd80100f7f3a75ba4c1a4310d1ef3cc344de2090
ChAmmar28 <niyazbekovammar@gmail.com>
2024-06-18 19:40:12 +0600

    update)

--- level 1.3/1/script.js
+++ level 1.3/1/script.js
@@ -1,13 +1,27 @@
 // Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.
 
+// // v1
+
+// let func = (x) => {
+//   if (x) {
+//     if (x.length > 1) {
+//       return x.at(-2);
+//     }
+//     return x;
+//   }
+//   return "esc";
+// };
+
+// // v2 (тут я забыл return и gpt напомнил)
+
+// let func = (x) => {
+//   x ? (x.length > 1 ? x.at(-2) : x) : "esc";
+// };
+
+// // v3 (gpt help)
+
 let func = (x) => {
-  if (x) {
-    if (x.length > 1) {
-      return x.at(-2);
-    }
-    return x;
-  }
-  return "esc";
+  return x ? (x.length > 1 ? x.at(-2) : x) : "esc";
 };
 
 console.log(func(prompt("ввод", "")));
