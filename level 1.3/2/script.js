615571ef269e71ab09da6d8a70f1179fcc0ed228
ChAmmar28 <niyazbekovammar@gmail.com>
2024-06-18 19:23:37 +0600

    update)

--- level 1.3/2/script.js
+++ level 1.3/2/script.js
@@ -1,10 +1,28 @@
 // Даны два целых числа. Проверьте, что первое число без остатка делится на второе.
 
+// v1
+
+// let func = (a, b) => {
+//   if (!(a.toFixed() % b.toFixed())) {
+//     return "ok";
+//   }
+//   return "-";
+// };
+
+// console.log(func(+prompt("1"), +prompt("2")));
+
+// v2 (gpt обьяснил что метод toFixed возвращаяет строку я про это забыл)
+
+// let func = (a, b) => {
+//   !(a.toFixed() % b.toFixed()) ? "ok" : "-";
+// };
+
+// console.log(func(+prompt("1"), +prompt("2")));
+
+// v2 gpt help
+
 let func = (a, b) => {
-  if (!(a.toFixed() % b.toFixed())) {
-    return "ok";
-  }
-  return "-";
+  !(Number(a.toFixed()) % Number(b.toFixed())) ? "ok" : "-";
 };
 
 console.log(func(+prompt("1"), +prompt("2")));
